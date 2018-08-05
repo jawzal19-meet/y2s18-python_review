ec497aaa57b5ab88cd1271d21bc40205ad3a7d89
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 11:58:40 +0300

    python

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,7 @@
 # Write your solution for 1.2 here!
+sum=0
+for i in range(1,101):
+    if i%2==0:
+        sum=sum+i
+print (sum)
+
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,7 @@
 # Write your solution for 1.1 here!
+sum =0 
+for i in range(1,101):
+    sum=sum+i
+print(sum)
+
+print('hello world')
